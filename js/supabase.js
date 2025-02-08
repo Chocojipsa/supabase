d8d85451621cb35d91ce4769239f8b03229fb607
rbxo0128 <104380956+rbxo0128@users.noreply.github.com>
2025-02-08 18:15:30 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
+const githubApiKey = process.env.supabasekey;
 const supabase = createClient(
     "https://qjjomzlttcwezludhrwt.supabase.co",
-     ""
+     githubApiKey
 );
 
 export { supabase };
