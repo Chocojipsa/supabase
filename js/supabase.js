277203ce9c7a7fefacf175f8e2202c7cee4e1a15
rbxo0128 <104380956+rbxo0128@users.noreply.github.com>
2025-02-08 18:47:41 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 require('dotenv').config();
 
-const githubApiKey = process.env.supabasekey;
+const githubApiKey = process.env.SUPABASE_KEY;
 const supabase = createClient(
     "https://qjjomzlttcwezludhrwt.supabase.co",
      githubApiKey
